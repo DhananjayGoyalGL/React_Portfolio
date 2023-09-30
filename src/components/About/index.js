6615622821c20aaaff6de03ccd54261ff28c4e18
Dhananjay Goyal <1406dhananjay@gmail.com>
2023-09-30 17:35:29 +0530

    commit msgg

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,7 @@
-import './index.scss'
-import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAngular,
   faCss3,
@@ -9,51 +9,63 @@ import {
   faHtml5,
   faJsSquare,
   faReact,
-} from '@fortawesome/free-brands-svg-icons'
-import Loader from 'react-loaders'
+} from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
 
 const About = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState("text-animate");
 
   useEffect(() => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 3000)
-  })
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  });
 
-  // Function to handle resume download
   const handleDownloadResume = () => {
-    const link = document.createElement('a')
-    link.href = '../DhananjayFullStack.pdf'
-    link.download = 'DhananjayFullStack.pdf'
-    link.click()
-  }
+    // Use the relative path to the PDF file in the public folder
+    const pdfPath = process.env.PUBLIC_URL + "/DhananjayFullStack.pdf";
+
+    // Create a link element
+    const link = document.createElement("a");
+    link.href = pdfPath;
+    link.download = "DhananjayFullStack.pdf";
+    link.click();
+  };
 
   return (
     <>
-      {' '}
+      {" "}
       <div className="container about-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+              strArray={["A", "b", "o", "u", "t", " ", "M", "e"]}
               idx={15}
             />
           </h1>
           <p className="para">
-            A highly motivated Full Stack Developer certified by 𝐈𝐈𝐓 - 𝐑𝐨𝐨𝐫𝐤𝐞𝐞.
-            I'm excited to apply my learnt skills in developing responsive and
-            intuitive interfaces using SPA (Single Page Application) and React
-            JS.
+            👋 Hey there! <br></br>Hope you are doing well... My name is Dhananjay
+            Goyal currently working as a Full Stack Software Developer Intern at
+            Linsible Technologies, where I'm passionate about crafting digital
+            experiences that make a real impact. 🚀 🌟{" "}
           </p>
           <p className="para">
-            I love building responsive and user-friendly web applications using
-            𝐅𝐫𝐨𝐧𝐭𝐞𝐧𝐝 𝐭𝐞𝐜𝐡𝐧𝐨𝐥𝐨𝐠𝐢𝐞𝐬 𝐬𝐮𝐜𝐡 𝐚𝐬 : HTML, CSS, JavaScript, React JS
+            🎓 I've honed my software development skills through a comprehensive
+            program at Great Learning, IIT Roorkee. 📚 Currently, I'm further
+            sharpening my expertise through Delta and Alpha courses in the MERN
+            stack and Data Structures and Algorithms (DSA) at Apna College. I
+            firmly believe that continuous learning is the key to staying at the
+            forefront of this dynamic field.{" "}
           </p>
           <p className="para">
-            𝐜𝐨𝐮𝐩𝐥𝐞𝐝 𝐰𝐢𝐭𝐡 𝐁𝐚𝐜𝐤𝐞𝐧𝐝 𝐭𝐞𝐜𝐡𝐧𝐨𝐥𝐨𝐠𝐢𝐞𝐬 𝐬𝐮𝐜𝐡 𝐚𝐬 : Spring, Hibernate, JPA,
-            Bootstrap, and MySQL.
+            💬 Feel free to
+            connect with me to discuss opportunities, share insights, or simply
+            chat about tech and innovation. You can reach me at:
+            <p>
+              📧 Email: 1406dhananjay@gmail.com <br></br>🌐 More about me:
+              https://linktr.ee/Dhananjay_Goyal
+            </p>
           </p>
           <button className="download-button" onClick={handleDownloadResume}>
             Download Resume
@@ -85,7 +97,7 @@ const About = () => {
       </div>
       <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,42 +1,42 @@
-import LogoTitle from '../../assets/images/logo-s.png'
-import { Link } from 'react-router-dom'
-import './index.scss'
-import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
-import final from '../../assets/images/final.gif'
-import Loader from 'react-loaders'
+import LogoTitle from "../../assets/images/logo-s.png";
+import { Link } from "react-router-dom";
+import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
+import final from "../../assets/images/final.gif";
+import Loader from "react-loaders";
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['D', 'h', 'a', 'n', 'a', 'n', 'j', 'a', 'y']
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["D", "h", "a", "n", "a", "n", "j", "a", "y"];
   const jobArray = [
-    'o',
-    'f',
-    't',
-    'w',
-    'a',
-    'r',
-    'e',
-    ' ',
-    'D',
-    'e',
-    'v',
-    'e',
-    'l',
-    'o',
-    'p',
-    'e',
-    'r',
-  ]
+    "o",
+    "f",
+    "t",
+    "w",
+    "a",
+    "r",
+    "e",
+    " ",
+    "D",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
+      setLetterClass("text-animate-hover");
+    }, 4000);
 
     return () => {
-      clearTimeout(timeoutId)
-    }
-  }, [])
+      clearTimeout(timeoutId);
+    };
+  }, []);
 
   return (
     <>
@@ -61,19 +61,16 @@ const Home = () => {
               idx={18}
             />
           </h1>
-          <h2>
-            {' '}
-            Fullstack Developer / Frontend Developer
-          </h2>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
+          <h2> Fullstack Developer / Frontend Developer</h2>
+          <Link to="/portfolio" className="flat-button">
+            MY WORK
           </Link>
         </div>
         <img className="gif" src={final} alt="gif" />
       </div>
       <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
